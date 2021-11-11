86a2d26f29b4d858c55a7401e9945b50c9ebfbac
Luke Wilson <l2wilson94@gmail.com>
2021-11-11 19:16:12 +0000

    WASD QE controls

--- script.js
+++ script.js
@@ -4,18 +4,25 @@ const context = canvas.getContext("2d")
 canvas.style["background-color"] = Colour.Black
 canvas.style["margin"] = 0
 
+
+const TIMELINE_LENGTH = 30
+const timeline = []
+for (let i = 0; i < TIMELINE_LENGTH; i++) {
+	timeline.push({number: i+1})
+}
+for (const time of timeline) {
+	const img = document.createElement("img")
+	img.src = `images/${time.number}.jpg`
+	time.img = img
+}
+
+const plane = document.createElement("img")
+plane.src = `images/plane.png`
+
 on.load(() => {
 	document.body.appendChild(canvas)
 	document.body.style["overflow"] = "hidden"
-	document.body.style["margin"] = "0px"
-	
-	
-	for (const time of timeline) {
-		const img = document.createElement("img")
-		img.src = `thumbs/${time.number}.jpg`
-		time.img = img
-	}
-	
+	document.body.style["margin"] = "0px"	
 	setInterval(tick, 1000 / 60)
 })
 
@@ -27,67 +34,38 @@ on.resize(() => {
 })
 
 trigger("resize")
-
-const TIMELINE_LENGTH = 30
-const timeline = []
-for (let i = 0; i < TIMELINE_LENGTH; i++) {
-	timeline.push({number: i+1})
-}
+	
+timeline[0].name = "2D Falling Sand"
 
 const tick = () => {
 	updateCamera()
 	draw()
 }
 
-const draw = () => {
-	context.clearRect(0, 0, canvas.width, canvas.height)
-	drawTimeline()
-}
-
-const BOTTOM_GAP = 50
-
-const drawTimeline = () => {
-	let x = camera.x
-	let y = canvas.height - BOTTOM_GAP
-	
-	for (const time of timeline) {
-		drawTime(time, x, y)
-		
-		if (time.number >= TIMELINE_LENGTH) break
-		
-		
-		context.strokeStyle = Colour.White
-		context.lineWidth = 30 * camera.scale
-		//context.setLineDash([10])
-		
-		context.beginPath()
-		context.moveTo(x, y)
-		
-		x += getDistance()
-		
-		context.lineTo(x, y)
-		context.closePath()
-		context.stroke()
-	}
-	
-}
-
-const getDistance = () => {
-	return timeline[0].img.width * 2 * camera.scale
-}
-
 const camera = {
-	scale: 0.5,
-	x: 720,
+	scale: 1.0,
+	x: 0,
 	dx: 0,
 	dscale: 1.0,
+	focus: 0,
+	dfocus: 0,
+}
+
+const zoom = (dscale) => {
+	const amount = camera.scale*dscale - camera.scale
+	camera.x -= amount * camera.focus
+	camera.scale += amount
 }
 
 const updateCamera = () => {
 	camera.x += camera.dx
-	camera.scale *= camera.dscale
+	camera.focus += camera.dfocus
+	zoom(camera.dscale)
 }
 
+const WASD_MOVE_SPEED = 10.0
+const WASD_ZOOM_SPEED = 0.01
+const WASD_FOCUS_SPEED = 10.0
 on.keydown(e => {
 	if (e.key === "ArrowRight") {
 		camera.dx -= 0.5
@@ -101,32 +79,91 @@ on.keydown(e => {
 	else if (e.key === "ArrowUp") {
 		camera.dscale += 0.001
 	}
+	
+	else if (e.key === "d") {
+		camera.dx = -WASD_MOVE_SPEED
+	}
+	else if (e.key === "a") {
+		camera.dx = WASD_MOVE_SPEED
+	}
+	else if (e.key === "w") {
+		camera.dscale = 1 + WASD_ZOOM_SPEED
+	}
+	else if (e.key === "s") {
+		camera.dscale = 1 - WASD_ZOOM_SPEED
+	}
+	
+	else if (e.key === "e") {
+		camera.dfocus = WASD_FOCUS_SPEED
+	}
+	
+	else if (e.key === "q") {
+		camera.dfocus = -WASD_FOCUS_SPEED
+	}
 })
 
-const getThumbHeight = () => {
-	const ratio = timeline[0].img.height / timeline[0].img.width
-	//let height = camera.scale * 2 * timeline[0].img.height
-	//canvas.height - (130*2)
-	return getThumbWidth() * ratio
+on.keyup(e => {
+	if (e.key === "d") {
+		if (camera.dx < 0) camera.dx = 0
+	}
+	else if (e.key === "a") {
+		if (camera.dx > 0) camera.dx = 0
+	}
+	else if (e.key === "w") {
+		if (camera.dscale > 1.0) camera.dscale = 1.0
+	}
+	else if (e.key === "s") {
+		if (camera.dscale < 1.0) camera.dscale = 1.0
+	}
+	
+	else if (e.key === "e") {
+		if (camera.dfocus > 0) camera.dfocus = 0
+	}
+	
+	else if (e.key === "q") {
+		if (camera.dfocus < 0) camera.dfocus = 0
+	}
+})
+
+const draw = () => {
+	context.clearRect(0, 0, canvas.width, canvas.height)
+	drawTimeline()
 }
 
-const getThumbWidth = () => {
-	let width = getDistance() * 0.98 - 40
-	return Math.min(width, 720)
-	//const height = getThumbHeight()
-	//const ratio = timeline[0].img.height / timeline[0].img.width
-	//return height / ratio
-}
-
-const drawTime = (time, x, y) => {
+const BOTTOM_GAP = 50
+const PLANE_SCALE = 0.15
+const drawTimeline = () => {
+	
+	const x = 0 + camera.x
+	const y = canvas.height - BOTTOM_GAP
+	const width = canvas.width * camera.scale
+	
+	context.strokeStyle = Colour.White
+	context.lineWidth = 8
 	context.beginPath()
-	context.arc(x, y, 50 * camera.scale, 0, 2*Math.PI)
+	context.moveTo(x, y)
+	context.lineTo(x + width, y)
+	context.stroke()
+	
+	for (const time of timeline) {
+		//drawTime(time)
+		
+	}
+	
+	const planeWidth = plane.width * PLANE_SCALE
+	const planeHeight = plane.height * PLANE_SCALE
+	context.drawImage(plane, camera.x + camera.scale * (camera.focus) - planeWidth/2, y - planeHeight * 0.45, planeWidth, planeHeight)
+	
+}
+
+const drawTime = (time) => {
+	
+	const gap = canvas.width / (timeline.length-1)
+	const x = (time.number-1) * gap + camera.x
+	const y = canvas.height - BOTTOM_GAP
+	
+	context.beginPath()
+	context.arc(x, y, 15, 0, 2*Math.PI)
 	context.fillStyle = Colour.White
 	context.fill()
-	
-	const height = getThumbHeight()
-	const width = getThumbWidth()
-	
-	context.drawImage(time.img, x - width/2, y - height * 1.2, width, height)
-	
 }
